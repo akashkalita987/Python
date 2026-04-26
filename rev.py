c79d7e673f8f8538865ef2beb514a9bceae01ab8
akashkalita987 <akashjyotikalita76@gmail.com>
2026-04-26 20:28:17 +0530

    rev uing different method

--- rev.py
+++ rev.py
@@ -1,3 +1,15 @@
+#reversing using reverse function
 college = ["avc", "cu", "bbc"]
 college.reverse()
-print(f"the reversd array is: {college}")
+print(f"the reversd array is: \n{college}")
+
+#reversing uisng slicing
+college1 = ["avc", "cu", "bbc"]
+print(college1[::-1])
+
+#reversing using loop
+college2  = ["avc", "cu", "bbc"]
+rev = []
+for i in range(len(college2) -1, -1, -1):
+    rev.append(college2[i])
+print(rev)
