782f9ef3e4317c6c83d090be187e719128bd5654
akashkalita987 <akashjyotikalita76@gmail.com>
2026-04-27 09:28:59 +0530

    .

---
+++ set1.py
@@ -0,0 +1,20 @@
+#set
+"""
+set1 = {1,2,3,4,5}
+set2 = {9,8,7,6,5,4,3,2,1}
+set2.add(10)
+set2.remove(9)
+print(set2)
+"""
+#list
+li = ["Akash","bikash",'A','B', 5, 17, True]
+li.append("hello bhai")
+reversed_li = []
+
+for item in li:
+    reversed_li.insert(0,item)
+#print(li[::-1])
+print(reversed_li)
+
+tup = ("hy")
+print(tup)
