d2846a5897155d6956c9aca0b1bdcb0e77389909
akashkalita987 <akashjyotikalita76@gmail.com>
2026-05-28 22:46:23 +0530

    createing new file

--- textFile.py
+++ textFile.py
@@ -1,3 +1,8 @@
 file = open("demo.txt", "r")
 print(file.read()) # Read and print file content
 file.close()
+
+file = open("demo1.txt","w")
+file.write("helloooooo")
+print(file.read())
+file.close()
