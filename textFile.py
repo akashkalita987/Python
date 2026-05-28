ec12a6b7894f6460cb1fe167bd39593dd05bc1ed
akashkalita987 <akashjyotikalita76@gmail.com>
2026-05-28 23:38:35 +0530

    solved

--- textFile.py
+++ textFile.py
@@ -1,4 +1,7 @@
-file = open("demo1.txt","w")
-file.write("helloooooo")
-print(file.read())
-file.close()
+# Step 1: Write to the file
+with open("demo1.txt", "w") as file:
+    file.write("helloooooo")
+
+# Step 2: Open it again to read
+with open("demo1.txt", "r") as file:
+    print(file.read())
