e4b41bf46664926be7861f5a7c4e086a2a3ffb64
akashkalita987 <akashjyotikalita76@gmail.com>
2026-06-01 18:41:15 +0530

    ready to use

--- cl_obj_try.py
+++ cl_obj_try.py
@@ -1,17 +1,12 @@
-class student:
-
-    def __init__(self, name, rollNo, cls):
-        self.name = name
-        self.rollNo = rollNo
-        self.cls = cls
-
-    def display(self):
-        return f"{self.name} {self.rollNo} {self.cls}"
-    
-student1 = student("akash",5,"4th sem")
-student2 = student("bikash",6,"2nd sem")
-
-print ("Student detailes are____")
-print(f"Student name:- {student1.name}")
-print(student1.display())
-
+num =int(input("enter the number"))
+even_sum = 0
+odd_sum = 0
+for numbers in range(1,num+1):
+    if numbers%2 == 0:
+        print(f"Even numbers are {numbers}")
+        even_sum +=numbers
+        
+    else: 
+        print(f"Odd numbers are {numbers}")
+        odd_sum += numbers
+print(f"Difference between sum of odd and even sum is\n{even_sum-odd_sum}")
