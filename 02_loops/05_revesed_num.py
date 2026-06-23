58e547683b6931b251c1bd0678a2e10b5557ff01
akashkalita987 <akashjyotikalita76@gmail.com>
2026-06-23 11:22:42 +0530

    done

---
+++ 02_loops/05_revesed_num.py
@@ -0,0 +1,19 @@
+# num = 12345
+# 
+# print("Reversed number: ", end="")
+# 
+# # Loop until the number becomes 0
+# while num > 0:
+#     # 1. Get the last digit
+#     last_digit = num % 10
+#     
+#     # 2. Print it immediately without jumping to a new line
+#     print(last_digit, end=" ")
+#     
+# # Print an empty line at the end just to clean up the terminal output
+# print()
+
+list1 = [1,2]
+list2 = [3,4]
+list1.extend(list2)
+print(list1)
