ff3e9a69ab95a381626b14db795aa20ed45cc29e
akashkalita987 <akashjyotikalita76@gmail.com>
2026-06-23 07:58:15 +0530

    done

--- 11_mili/03_sum_harmonic.py
+++ 11_mili/03_sum_harmonic.py
@@ -1,8 +1,6 @@
 # Question 3: Sum the series \sum_{k=1}^{100} 1/k
 
 series_sum = 0.0
-
 for k in range(1, 101):
     series_sum += 1 / k
-
 print(f"The sum of the series from k=1 to 100 is: {series_sum:.6f}")
