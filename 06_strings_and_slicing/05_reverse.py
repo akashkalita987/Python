9e0fc2a64a8c96cedc309fc6bee8245681dfe379
akashkalita987 <akashjyotikalita76@gmail.com>
2026-06-24 18:47:37 +0530

    .

--- 06_strings_and_slicing/05_reverse.py
+++ 06_strings_and_slicing/05_reverse.py
@@ -1,17 +1,17 @@
- ##reversing using reverse function
- #college = ["avc", "cu", "bbc"]
- #college.reverse()
- #print(f"the reversd array is: \n{college}")
- #
- ##reversing uisng slicing
- #college1 = ["avc", "cu", "bbc"]
- #print(college1[::-1])
- #
- ##reversing using loop
- #college2  = ["avc", "cu", "bbc", "dc", "lcb", "pc", "bc"]
- #rev = []
- #for i in range(len(college2) -1, -1, -1):
- #    rev.append(college2[i])
- #print(rev)
+#reversing using reverse function
+college = ["avc", "cu", "bbc"]
+college.reverse()
+print(f"the reversd array is: \n{college}")
+
+#reversing uisng slicing
+college1 = ["avc", "cu", "bbc"]
+print(college1[::-1])
+
+#reversing using loop
+college2  = ["avc", "cu", "bbc", "dc", "lcb", "pc", "bc"]
+rev = []
+for i in range(len(college2) -1, -1, -1):
+    rev.append(college2[i])
+print(rev)
 college2  = ["avc", "cu", "bbc", "dc", "lcb", "pc", "bc"]
 print(college2[-1:-1:-1])
